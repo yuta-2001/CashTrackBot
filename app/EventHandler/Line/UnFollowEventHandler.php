d9852fb45671c0c62d37ff9a2d54dd389e687eeb
yuta-sugi <sugimine.yuta@codecomplete.com>
2023-11-09 22:04:13 +0700

    アンフォロー時にユーザー情報削除

---
+++ app/EventHandler/Line/UnFollowEventHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventHandler\Line;
+
+use App\EventHandler\EventHandler;
+use App\Models\User;
+use LINE\Clients\MessagingApi\Api\MessagingApiApi;
+use LINE\Webhook\Model\UnFollowEvent;
+
+class UnFollowEventHandler implements EventHandler
+{
+    private $bot;
+    private $event;
+
+    public function __construct(MessagingApiApi $bot, UnFollowEvent $event)
+    {
+        $this->bot = $bot;
+        $this->event = $event;
+    }
+
+    public function handle()
+    {
+        $userId = $this->event->getSource()['userId'];
+
+        User::where('line_user_id', $userId)->delete();
+
+        \Log::info('アカウント削除実行');
+        \Log::info('userId:' . $userId);
+    }
+}
