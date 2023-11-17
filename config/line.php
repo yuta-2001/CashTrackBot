8704bb5ea0865919fd94828724747c4b2557ae8e
yuta-2001 <yu0210ta.sug@gmail.com>
2023-11-17 16:05:32 +0700

    fix

--- config/line.php
+++ config/line.php
@@ -20,7 +20,6 @@ return [
     'liff_ids' => [
         'opponent_create' => env('LIFF_OPPONENT_CREATE_LIFF_ID'),
         'opponent_edit' => env('LIFF_OPPONENT_EDIT_LIFF_ID'),
-        'lending_and_borrowing_create' => env('LIFF_LENDING_AND_BORROWING_CREATE_LIFF_ID'),
     ],
 
     'explanation' => [
