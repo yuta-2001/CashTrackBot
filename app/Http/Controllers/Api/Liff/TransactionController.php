a0d869c3d3bae6cb10715976e1aa6d8f81ce7baa
yuta-2001 <yu0210ta.sug@gmail.com>
2023-11-17 15:00:12 +0700

    API作成

---
+++ app/Http/Controllers/Api/Liff/TransactionController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api\Liff;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TransactionResource;
+use App\Http\Requests\Transaction\StoreRequest;
+use App\Http\Requests\Transaction\UpdateRequest;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index(Request $request)
+    {
+        $user = $request->attributes->get('user');
+        $transactions = Transaction::where('user_id', $user->id)->get();
+
+        return TransactionResource::collection($transactions);
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $user = $request->attributes->get('user');
+        $data = $request->validated();
+        $data['user_id'] = $user->id;
+        $transaction = Transaction::create($data);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'success',
+            'data' => new TransactionResource($transaction),
+        ], 200);
+    }
+
+    public function update(UpdateRequest $request, int $id)
+    {
+        $user = $request->attributes->get('user');
+        $transaction = Transaction::where('user_id', $user->id)->where('id', $id)->first();
+        $data = $request->validated();
+        $transaction->update($data);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'success',
+            'data' => new TransactionResource($transaction),
+        ], 200);
+    }
+
+    public function delete(Request $request, int $id)
+    {
+        $user = $request->attributes->get('user');
+        $transaction = Transaction::where('user_id', $user->id)->where('id', $id)->first();
+        $transaction->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'success',
+        ], 200);
+    }
+}
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.liff' => \App\Http\Middleware\AuthenticateWithAccessToken::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
